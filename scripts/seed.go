1bebd56f162ebe7cefa9cdbf6f65950dcab3b012
Milad Rasouli <milad75rasouli@gmail.com>
2024-07-09 12:43:26 +0330

    bug: fix seed.go

--- scripts/seed.go
+++ scripts/seed.go
@@ -1,115 +1,40 @@
-package fixtures
+package main
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
-	"math/rand"
+	"log"
 	"time"
 
 	"github.com/the-SkillConnect/SkillConnect/db"
+	"github.com/the-SkillConnect/SkillConnect/db/fixtures"
 )
 
-var (
-	Categories = [...]string{
-		"tech",
-		"database",
-		"S3",
-		"GraphQL",
-		"GRPC",
-	}
-)
-
-func AddUserIdentity(store db.Queries, ctx context.Context, i int) error {
-	arg := db.InsertUserIdentityParams{
-		Email:         fmt.Sprintf("user%d@james.com", i),
-		Password:      fmt.Sprintf("abcd%d%d%d%d", i, i, i, i),
-		FirstName:     fmt.Sprintf("user%d", i),
-		Surname:       fmt.Sprintf("james%d", i),
-		MobilePhone:   fmt.Sprintf("%d%d%d%d%d%d%d", i, i, i, i+6, i*2, i+3, i+2),
-		WalletAddress: fmt.Sprintf("secureWallet%d%d%d%d%d%d%d", i, i, i, i+6, i*2, i+3, i+2),
-		CreatedAt:     time.Now(),
-		UpdatedAt:     time.Now(),
-	}
-	_, err := store.InsertUserIdentity(ctx, arg)
-	return err
-}
-
-func AddUserProfile(store db.Queries, ctx context.Context, i int) error {
-	arg := db.InsertUserProfileParams{
-		UserID:       int64(i),
-		Rating:       int64(rand.Intn(5)),
-		Description:  sql.NullString{String: fmt.Sprintf("this is a mock description for user %d", i), Valid: true},
-		DoneProject:  int64(rand.Intn(20)),
-		GivenProject: int64(rand.Intn(100)),
-		CreatedAt:    time.Now(),
-		UpdatedAt:    time.Now(),
-	}
-	_, err := store.InsertUserProfile(ctx, arg)
-	return err
-}
-
-func AddUserRecommendation(store db.Queries, ctx context.Context, i int) error {
-	rID := rand.Intn(i) + 1
-	arg := db.InsertUserRecommendationParams{
-		GivenID:     int64(i),
-		ReceivedID:  int64(rID),
-		Description: fmt.Sprintf("this is recom from user%d to user%d", int64(i), int64(rID)),
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-	_, err := store.InsertUserRecommendation(ctx, arg)
-	return err
-}
-
-func AddProject(store db.Queries, ctx context.Context, i int) error {
-	rID := rand.Intn(i) + 1
-	arg := db.InsertProjectParams{
-		Description: fmt.Sprintf("this is a mock mock mock mock mock project from user%d", int64(i)),
-		Title:       fmt.Sprintf("this is a mock project title from user%d", int64(i)),
-		TotalAmount: fmt.Sprintf("%.3f", rand.Float64()*float64(i*10)),
-		DoneStatus:  sql.NullBool{Bool: false, Valid: true},
-		UserID:      int64(rID),
-		Fee:         fmt.Sprintf("%.3f", rand.Float64()*float64(i)),
-		CategoryID:  int64(rand.Intn(len(Categories) + 1)),
-		CreatedAt:   time.Now(),
-		UpdatedAt:   time.Now(),
-	}
-	_, err := store.InsertProject(ctx, arg)
-	return err
-}
-
-func AddComment(store db.Queries, ctx context.Context, i int) error {
-	rID := rand.Intn(i) + 1
-	arg := db.InsertCommentParams{
-		UserID:    int64(i),
-		ProjectID: int64(rID),
-		Date:      time.Now(),
-		Text:      fmt.Sprintf("this a random %d comment", i),
-	}
-	_, err := store.InsertComment(ctx, arg)
-	return err
-}
-
-func AddAssignProject(store db.Queries, ctx context.Context, i int) error {
-	rID := rand.Intn(i) + 1
-
-	arg := db.InsertAssignProjectParams{
-		UserID:    int64(i),
-		ProjectID: int64(rID),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-	_, err := store.InsertAssignProject(ctx, arg)
-	return err
-}
-
-func AddCategory(store db.Queries, ctx context.Context) error {
-	for _, value := range Categories {
-		_, err := store.InsertCategory(ctx, value)
-		if err != nil {
-			return err
+func main() {
+	now := time.Now()
+	var i int
+	go func() {
+		for {
+			<-time.After(time.Millisecond * 500)
+			log.Printf("Seed Elapsed: %v, inserted: %d\n", time.Since(now), i)
 		}
+	}()
+	dbInstance, err := db.InitDB()
+	if err != nil {
+		log.Fatal(err)
 	}
-	return nil
+	db.TearDown(dbInstance)
+	db.CreateTables(dbInstance)
+	ctx := context.Background()
+	instance := db.New(dbInstance)
+	fixtures.AddCategory(*instance, ctx)
+
+	for i = 1; i < 10; i++ {
+		fixtures.AddUserIdentity(*instance, ctx, i)
+		fixtures.AddUserProfile(*instance, ctx, i)
+		fixtures.AddUserRecommendation(*instance, ctx, i)
+		fixtures.AddProject(*instance, ctx, i)
+		fixtures.AddComment(*instance, ctx, i)
+		fixtures.AddAssignProject(*instance, ctx, i)
+	}
+	log.Printf("Seed Ended in %v\n", time.Since(now))
 }
